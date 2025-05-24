616ae72d5b7cde338eda809216d27b88aa293b5c
QuantiumCodeV <quantiumcode@mail.ru>
2025-05-24 12:53:08 +0500

    fix

--- app/Filament/Resources/StatsResource.php
+++ app/Filament/Resources/StatsResource.php
@@ -316,7 +316,7 @@ class StatsResource extends Resource
                                 SELECT 
                                     CASE 
                                         WHEN (SELECT count FROM total_accounts) = 0 THEN 0
-                                        ELSE SUM(stats_invites_count)::float / (SELECT count FROM total_accounts)
+                                        ELSE CAST(SUM(stats_invites_count) AS DECIMAL(10,2)) / CAST((SELECT count FROM total_accounts) AS DECIMAL(10,2))
                                     END as avg
                                 FROM invite_accounts ia
                                 WHERE ia.invite_vendor_id = (SELECT id FROM invite_vendor_id)
@@ -333,9 +333,9 @@ class StatsResource extends Resource
                                 {$geoCondition}
                             )
                             SELECT 
-                                (SELECT count FROM total_accounts) * 
-                                (SELECT avg FROM avg_invites) * 
-                                (SELECT price FROM avg_price)
+                                CAST((SELECT count FROM total_accounts) AS DECIMAL(10,2)) * 
+                                CAST((SELECT avg FROM avg_invites) AS DECIMAL(10,2)) * 
+                                CAST((SELECT price FROM avg_price) AS DECIMAL(10,2))
                         ) {$direction}", $params);
                     }),
 
